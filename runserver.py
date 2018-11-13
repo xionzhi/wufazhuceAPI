f66018c6d2cfeb5900dccff87e332caab73172c5
xionz_ <411272132@qq.com>
2018-11-13 14:59:52 +0800

    tornado

--- runserver.py
+++ runserver.py
@@ -9,3 +9,13 @@ from service import app
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
+
+
+# from tornado.wsgi import WSGIContainer
+# from tornado.httpserver import HTTPServer
+# from tornado.ioloop import IOLoop
+# from service import app
+
+# http_server = HTTPServer(WSGIContainer(app))
+# http_server.listen(8080, address="127.0.0.1")
+# IOLoop.instance().start()
